8dfe150840517b6a22b8e1055968c8d67cf648d9
JaredJose <Jjose3490>
2020-09-15 21:40:48 -0700

    Initial check-in of module LexParserWIP

---
+++ LexParserWIP/LexicalParser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+namespace LexParserWIP
+{
+    public class LexicalParser
+    {
+        Stream textStream;
+        
+        String[] Operators = new String[30] { "=", "<=", "<", ">", ">=", "!=", "?", "+", "?", ";", "-", "=", "*", "/",
+        "++", "--", "+=", "-=", "*=", "/=", "&&", "||", "~", "!", "&", "&=", "|=", "~=", "^", "^="};
+        string[] keywords = { "define", "thing", "template", "point", "int", "double", "float", "short", "byte",  "ushort", "sbyte", "uint"};
+
+        public LexicalParser(Stream textStream)
+        {
+            this.textStream = textStream;
+        }
+
+        public Token GetNext()
+        {
+            TokenCode tokType = TokenCode.Operator;
+            StreamReader sr = new StreamReader(textStream);
+            String finalTokenStr = "";
+            char firstChar;
+            char nextLetter;
+            do
+            {
+                firstChar = (char)sr.Read();
+            } while (firstChar == ' ');
+            finalTokenStr += firstChar;
+            if (Char.IsLetter(firstChar) || firstChar.Equals('_'))
+            {
+                tokType = TokenCode.Identifier;
+            }
+            if (Char.IsDigit(firstChar))
+            {
+                tokType = TokenCode.Number;
+            }
+            
+            switch (tokType)
+            {
+                case TokenCode.Identifier:
+                    while((nextLetter = (char)sr.Read()) != ' ')
+                    {
+                        if (Char.IsLetterOrDigit(nextLetter))
+                        {
+                            finalTokenStr += nextLetter;
+                        }
+                        else
+                        {
+                            firstChar = nextLetter;
+                            break;
+                        }
+                    }
+                    break;
+                case TokenCode.Number:
+                    while((nextLetter = (char)sr.Read()) != ' ')
+                    {
+                        if(Char.IsDigit(nextLetter) || nextLetter.Equals('.'))
+                        {
+                            finalTokenStr += nextLetter;
+                        }
+                        else
+                        {
+                            firstChar = nextLetter;
+                            break;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+            foreach(String keyword in keywords)
+            {
+                if (finalTokenStr.Equals(keyword))
+                {
+                    tokType = TokenCode.Keyword;
+                }
+            }
+            Token retToken = new Token(finalTokenStr, tokType);
+            return retToken;
+        }
+    }
+}
---
+++ LexParserWIP/Token.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace LexParserWIP
+{
+    public enum TokenCode
+    {
+        Keyword,
+        Identifier,
+        Number,
+        Delimiter,
+        Operator
+    }
+    public class Token
+    {
+        private string _str;
+        private TokenCode _code;
+
+        public Token(string str, TokenCode code)
+        {
+            _str = str;
+            _code = code;
+        }
+
+        public string TokenString()
+        {
+            return _str;
+        }
+
+        public TokenCode GetTokCode()
+        {
+            return _code;
+        }
+    }
+}
